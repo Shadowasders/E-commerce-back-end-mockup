d94ae9c056f9094bc571645bc2c9689ec93f90d3
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-07-20 15:30:02 -0400

    updated product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag]
+  }).then((data) => {
+    res.json(data);
+  })
+  .catch((err) => {
+    res.status(500).json(err)
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      is: req.params.id,
+    },
+    include: [ Category, Tag]
+  }).then((data) => {
+    res.json(data);
+  })
+  .catch((err) => {
+    res.status(500).json(err)
+  });
 });
 
 // create new product
@@ -94,6 +113,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((data) => {
+    res.json(data);
+  })
+  .catch((err) => {
+    res.status(500).json(err)
+  });
 });
 
 module.exports = router;
